2797e75ff7d8d3d533dddf10d8088d59be46e315
CCchao888 <wjc510175817@163.com>
2023-10-30 14:08:25 +0800

    加入model模块的实体类、公共枚举类等

---
+++ model/src/main/java/me/thinkchao/tckt/vo/live/LiveVisitorQueryVo.java
@@ -0,0 +1,13 @@
+package me.thinkchao.tckt.vo.live;
+
+import lombok.Data;
+import io.swagger.annotations.ApiModelProperty;
+
+@Data
+public class LiveVisitorQueryVo {
+	
+	@ApiModelProperty(value = "直播课程id")
+	private Long liveCourseId;
+
+}
+
